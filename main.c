79cbdbd1d996b0ea84b82d086a83a7ee8a738f4d
流光丶 <harparker@outlook.com>
2025-11-24 14:56:35 +0800

    I want to hear some mottos

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-    printf("Enter a number: ");
-    int number;
-    scanf("%d", &number);
-    printf("You entered: %d\n", number);
+    char string[100];
+    printf("Enter a motto:");
+    fgets(string, sizeof(string), stdin);
+    printf("Your motto is: %s", string);
     return 0;
 }
